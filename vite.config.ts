25910bf58dd209028b2402dfa820074d86a7120b
zhrk <zshchipets@gmail.com>
2023-02-12 00:29:14 +0300

    migrate from cra to vite

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable import/no-extraneous-dependencies */
+
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import checker from 'vite-plugin-checker';
+import svgrPlugin from 'vite-plugin-svgr';
+
+export default defineConfig({
+  build: { outDir: 'build' },
+  server: { open: true, port: 3000, host: true },
+  plugins: [
+    react(),
+    checker({ typescript: true, overlay: { panelStyle: 'max-height: none; height: 100%;' } }),
+    svgrPlugin({ svgrOptions: { ref: true } }),
+  ],
+});
